f7e7dfd54d70b83c3059740f9663dbc458967172
Aryan Kumar <aryan.kumar9791@gmail.com>
2025-07-31 22:11:10 +0530

    Set up basic adding functionality

---
+++ app.js
@@ -0,0 +1,37 @@
+const newTask = document.getElementById("newTask");
+const addTask = document.getElementById("addTask");
+const taskList = document.getElementById("taskList");
+
+addTask.addEventListener("click", function (){ 
+  let value = newTask.value;
+  if(value.trim() === "") return;
+
+  const span = document.createElement("span");
+  span.textContent = value;
+  span.classList.add("task-text");
+
+  const complete_btn = document.createElement("button");
+  const edit_btn = document.createElement("button");
+  const del_btn = document.createElement("button");
+
+  complete_btn.textContent = '✔';
+  edit_btn.textContent = '✎';
+  del_btn.textContent = '🗑';
+  complete_btn.classList.add("complete-btn");
+  edit_btn.classList.add("edit-btn");
+  del_btn.classList.add("del-btn");
+
+  const taskItem = document.createElement("li");
+  taskItem.classList.add("taskItem");
+  taskItem.appendChild(span);
+  taskItem.appendChild(complete_btn);
+  taskItem.appendChild(edit_btn);
+  taskItem.appendChild(del_btn);
+
+  taskList.appendChild(taskItem);
+});
+
+complete_btn.addEventListener("click", function() {
+  taskList.classList.toggle("completed");
+});
+
