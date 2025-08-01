f4ce8d9538aaf30f0defbf755a7df09a3ea932ef
Aryan Kumar <aryan.kumar9791@gmail.com>
2025-08-01 15:59:30 +0530

    Completed basic button functionality

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
+let taskArray = [];
 const newTask = document.getElementById("newTask");
 const addTask = document.getElementById("addTask");
 const taskList = document.getElementById("taskList");
 
-addTask.addEventListener("click", function (){ 
-  let value = newTask.value;
-  if(value.trim() === "") return;
+function createTaskItem(txt, isComplete, isOnLoad){ 
+  if(txt.trim() === "") return;
+
+  if (!isOnLoad) {
+    const taskObj = {
+      text: txt,
+      completed: false,
+    };
+    taskArray.push(taskObj);
+    localStorage.setItem("tasks", JSON.stringify(taskArray));
+  }
 
   const span = document.createElement("span");
-  span.textContent = value;
+  span.textContent = txt;
   span.classList.add("task-text");
 
   const complete_btn = document.createElement("button");
@@ -30,12 +39,24 @@ addTask.addEventListener("click", function (){
 
   taskList.appendChild(taskItem);
 
+  if (isComplete) taskItem.classList.add("completed");
+
   complete_btn.addEventListener("click", function() {
     taskItem.classList.toggle("completed");
+    const idx = Array.from(taskList.children).indexOf(taskItem);
+    if (idx > -1) {
+      taskArray[idx].completed = !taskArray[idx].completed;
+      localStorage.setItem("tasks", JSON.stringify(taskArray));
+    }
   });
 
   del_btn.addEventListener("click", function (){
-    taskItem.remove();
+    const idx = Array.from(taskList.children).indexOf(taskItem);
+    if(idx > -1){
+      taskArray.splice(idx, 1);
+      localStorage.setItem("tasks", JSON.stringify(taskArray));
+      taskItem.remove();
+    }
   });
 
   edit_btn.addEventListener("click", function(){
@@ -49,11 +70,51 @@ addTask.addEventListener("click", function (){
     }
     else if(edit_btn.textContent === "💾"){
       span.textContent = input.value;
+      if(input.value.trim = "") return;
+      const idx = Array.from(taskList.children).indexOf(taskItem);
+      if(idx > -1){
+        taskArray[idx].text = input.value
+        localStorage.setItem("tasks", JSON.stringify(taskArray));
+      }
+
       taskItem.replaceChild(span, input);
       edit_btn.textContent = "✎";
     }
   });
+}
+
+addTask.addEventListener("click", function() {createTaskItem(newTask.value, false, false)});
+
+window.addEventListener("load", function () {
+  const storedTasks = this.localStorage.getItem("tasks");
+  if(storedTasks){
+    taskArray = JSON.parse(storedTasks);
+    taskArray.forEach(taskObj => {
+      createTaskItem(taskObj.text, taskObj.completed, true);
+    });
+  }
 });
 
+const compTaskBtn = document.getElementById("compTask");
+compTaskBtn.addEventListener("click", function() {
+  const listArray = Array.from(taskList.children);
+  listArray.forEach(function(taskItem, index) {
+    taskItem.classList.add("completed");
+    taskArray[index].completed = true;
+  });
+  localStorage.setItem("tasks",JSON.stringify(taskArray));
+});
 
+const remCompTask = document.getElementById("remCompTask");
+remCompTask.addEventListener("click", function (){
+  const listArray = Array.from(taskList.children);
+  listArray.forEach((taskItem) => {
+    if(taskItem.classList.contains("completed")){
+      taskItem.remove();
+    }
+  });
+  taskArray = taskArray.filter((task) => !task.completed);
+
+  localStorage.setItem("tasks", JSON.stringify(taskArray));
+});
 
